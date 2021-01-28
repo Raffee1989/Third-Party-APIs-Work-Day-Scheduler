742a24f877bc0318e0983922b8d720a0d2f091dd
Raffee <Raffee1989@gmail.com>
2021-01-27 17:41:25 -0800

    Added Local Storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,31 +2,47 @@
 var currentDay = moment().format("MM DD YYYY");
 $("#currentDay").text(currentDay);
 
+
 // Selecting the container by its class. 
 var container = $(".container");
 
-// Array of times to be placed in each row. 
-var eachHour = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+// // Array of times to be placed in each row. 
+var eachHour = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 // Looping through array made above. 
 for (let i = 0; i < eachHour.length; i++) {
 
-    // Variable called "row" to create containing (time block, inputs and button). 
+    // Creating rows which contains "div/textarea/button" elements inside. 
     var row = $("<div>").addClass("row");
 
-    // Variable called "timeBlock" to display the array hours in our time block div
-    var timeBlock = $("<div>").addClass("time-block hour col-1").text(eachHour[i]); 
+    // To display each hour in our time block divs
+    var timeBlock = $("<div>").addClass("col-1 time-block ").text(eachHour[i]);
 
-    // Variable called "inputArea" to create inputarea for user. 
-    var inputArea = $("<input placeholder= 'Add your event here!'></input>").addClass("col-10");
+    // Creating textarea for user inputs
+    var eventArea = $("<textarea placeholder = 'Event'>").addClass("col-10 hour ").attr("id", eachHour[i]);
 
     // Variable called "saveButton" created to save our inputs. 
     var saveButton = $("<button>").addClass("saveBtn col-1").text("Save");
 
-    // Displaying or appending "rows" created above inside "container".
+    // Appending "rows"
     container.append(row);
 
-    // Displaying or appending "timeBlock", "textArea", "button".
-    row.append(timeBlock, inputArea, saveButton);
+    // Appending "div/textarea/button" inside "row"
+    row.append(timeBlock, eventArea, saveButton);
+}
+
+// Inputs of textarea will be saved in local storage.
+$(".saveBtn").on("click", function () {
+    
+    var textContent = $(this).prev().val(); 
+    var timeEl = $(this).prev().prev().text(); 
+    localStorage.setItem(timeEl, textContent);
+});
+
+// Inputs of textarea will be saved and displayed
+for (var t = 0; t < eachHour.length; t++) {
+    var timeId = document.getElementById(eachHour[t]); 
+    var getInput = localStorage.getItem(eachHour[t])  
+    timeId.textContent = getInput
 }
 
